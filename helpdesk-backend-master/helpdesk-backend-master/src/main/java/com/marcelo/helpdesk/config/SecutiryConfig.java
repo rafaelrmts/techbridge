c9cfefd19851a82301e56e94d55c1ada00efc596
Rafael Rodrigues <118307734+rafaelrmts@users.noreply.github.com>
2025-05-05 09:44:25 -0300

    fix 2

--- helpdesk-backend-master/helpdesk-backend-master/src/main/java/com/marcelo/helpdesk/config/SecutiryConfig.java
+++ helpdesk-backend-master/helpdesk-backend-master/src/main/java/com/marcelo/helpdesk/config/SecutiryConfig.java
@@ -1,6 +1,7 @@
 package com.marcelo.helpdesk.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
